b6c472cb6788e002b3bec45474b4234dacbf1bea
NielMc <nielmce@gmail.com>
2020-01-03 17:37:02 +0000

    correct order of js files

--- 02-ConsumingAPIsUsingJavaScript/02-json.parse/main.js
+++ 02-ConsumingAPIsUsingJavaScript/02-json.parse/main.js
@@ -4,13 +4,9 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-}
-
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
+        data = this.responseText;
     };
 }
 
--- 02-ConsumingAPIsUsingJavaScript/03-getting_functional/main.js
+++ 02-ConsumingAPIsUsingJavaScript/03-getting_functional/main.js
@@ -4,9 +4,13 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
+function setData(jsonData) {
+    data = jsonData;
+}
+
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
+        setData(JSON.parse(this.responseText));
     };
 }
 
